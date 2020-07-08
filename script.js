f203bff49f51721e0ba76eed122d55336ce54fc9
Sergio <abc84@ukr.net>
2020-07-08 19:17:00 +0300

    rrr

---
+++ script.js
@@ -0,0 +1,37 @@
+
+//alert("i" + "will" + "be" + "developer");
+//let result = confirm("Are you here?");
+//console.log(result);
+
+// let result = +prompt("Where are you babe?", "here");
+// console.log(result);
+
+// const answers = [];
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Сколько Вам лет?', '');
+// answers[2] = prompt('Как ваша фамилия', '');
+// document.write(answers);
+// console.log(typeof(answers));
+
+// let incr = 9,
+// decr = 9;
+// ++incr;
+// decr--;
+
+// console.log(incr);
+
+// console.log(2+3 == 5);
+
+// const isChecked = true,
+//       isClosed = false;
+//       console.log(isChecked && isClosed);
+// const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+//     for(let i = 1; i <= 1000000; i++) {
+//       if ( (2 + 2) != 4) {
+//         console.log('Something has gone very wrong :( ');
+//       }
+//     }
+//   };
+const area = 24;
+const prot =23;
+const pot = 3;

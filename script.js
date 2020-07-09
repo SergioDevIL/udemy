5df482f2213cd00134877d8c12c82d0d8d47fb4f
Sergio <abc84@ukr.net>
2020-07-09 15:14:28 +0300

    First task(adding data to object)

--- script.js
+++ script.js
@@ -32,6 +32,21 @@
 //       }
 //     }
 //   };
-const area = 24;
-const prot =23;
-const pot = 3;
+// MY first Udemy Project
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '1');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies : {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);

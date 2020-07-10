776f407f8ef878866556f45f92dcca1eea53053f
Sergio <abc84@ukr.net>
2020-07-10 18:51:12 +0300

    Loops and ifs

--- script2.js
+++ script2.js
@@ -1,32 +1,53 @@
 "use strict";
 
-const area = 1200;
+// const area = 1200;
 
-if(area<1000) {
-    console.log('be good');
+// if(area<1000) {
+//     console.log('be good');
+// }
+// else if(area>1300) {
+//     console.log('my destiny');
+// } else {
+//     console.log('i will be here');
+// }
+
+// const product = 'Bro';
+
+// switch(product) {
+//     case 'Bread':
+//         console.log('its not my favourite product');
+//         break;
+//     case 'Essen':
+//         console.log('i dont like to eat');
+//         break;
+//     case 'Kuchen':
+//         console.log('i like it but its not true');
+//         break;
+//     case 'Brot':
+//         console.log('i like Brot');
+//         break;
+//     default: console.log('no no no');
+//     break;
+// }
+
+/*===Циклы====*/
+
+
+
+let num = 34;
+
+// while(num<50) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num<50);
+
+for(let i = 1; i < 9; i++) {
+    console.log(num);
+    num++;
 }
-else if(area>1300) {
-    console.log('my destiny');
-} else {
-    console.log('i will be here');
-}
-
-const product = 'Bro';
-
-switch(product) {
-    case 'Bread':
-        console.log('its not my favourite product');
-        break;
-    case 'Essen':
-        console.log('i dont like to eat');
-        break;
-    case 'Kuchen':
-        console.log('i like it but its not true');
-        break;
-    case 'Brot':
-        console.log('i like Brot');
-        break;
-    default: console.log('no no no');
-    break;
-}
-
